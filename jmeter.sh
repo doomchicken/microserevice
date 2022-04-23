d2f16c374f6636636e60ab2c65bb0c69d2b426f4
gregoryfavish <gfdoomchicken@gmail.com>
2022-04-23 15:15:19 +0200

    more jmeter

--- jmeter.sh
+++ jmeter.sh
@@ -1,1 +1,6 @@
- rm -f test_log && rm -f test_output && JVM_ARGS="-Xms4000m -Xmx4000m" jmeter -n -t Spring\ Reactive.jmx -l test_log.jtl -o ./testreport
+ rm -f test_log.jtl  \
+&& rm -f jmeter.log  \
+&& rm -f -r test-report  \
+&& mkdir test-report  \
+&& JVM_ARGS="-Xms8000m -Xmx8000m"  \
+jmeter -n -t Spring\ Reactive.jmx -l test_log.jtl -e -o /Users/gregoryfavish/dev/microsrevice\ 2/test-report
