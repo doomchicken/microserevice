7f0a3e8c949a8f2fe176d4202974ae498c9a46d3
gregoryfavish <gfdoomchicken@gmail.com>
2022-04-24 19:37:21 +0200

    makng test run sequentially

--- jmeter.sh
+++ jmeter.sh
@@ -3,4 +3,4 @@
 && rm -f -r test-report  \
 && mkdir test-report  \
 && JVM_ARGS="-Xms8000m -Xmx8000m"  \
-jmeter -n -t Spring\ Reactive.jmx -l test_log.jtl -e -o /Users/gregoryfavish/dev/microsrevice\ 2/test-report
+jmeter -n -t Jmeter\ Test\ Plan.jmx -l test_log.jtl -e -o /Users/gregoryfavish/dev/microsrevice\ 2/test-report
